4ec10108f85e69142bde9496ebb670786e18eb30
irving ko <irving.ko@hotmail.com>
2021-05-14 15:13:14 +0800

    first

---
+++ Api/SendMessageController.cs
@@ -0,0 +1,47 @@
+﻿using WebSite.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Mail;
+using System.Web.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace WebSite.Api {
+	public class SendMessageController : ApiController {
+		[Route("api/SendMessage")]
+		[HttpPost]
+		public void POST(TouchData data) {
+			Task.Run(() => {
+				try {
+					ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(ValidateServerCertificate);
+					SmtpClient client = new SmtpClient("smtp.office365.com", 587);
+					client.EnableSsl = true;
+					client.Credentials = new NetworkCredential("hsinchen@chenscode.com.tw", "Ji35j04fu06");
+
+					MailAddress from = new MailAddress("hsinchen@chenscode.com.tw", "web touch");
+					MailAddress to = new MailAddress("hsinchen@chenscode.com.tw");
+					MailMessage message = new MailMessage(from, to);
+					message.Subject = data.Subject;
+					message.Body = $"{data.Msg}\r\n\r\nfrom:{data.Name}\r\nemail:{data.Email}";
+					client.SendMailAsync(message);
+				} catch (Exception e) {
+					
+				}
+			});
+		}
+
+		[Route("api/Test")]
+		[HttpGet]
+		public bool Test() {
+			return true;
+        }
+
+		private bool ValidateServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) {
+			return true;
+		}
+	}
+}
---
+++ js/ainos.js
@@ -0,0 +1,40 @@
+﻿$('#btnContactUs').click(function () {
+    $('#btnContactUs').attr('disabled', true);
+    var touchData = {
+        Name: $('#name').val(),
+        Email: $('#email').val(),
+        Subject: $('#subject').val(),
+        Msg: $('#msg').val(),
+
+    }
+    $.ajax({
+        url: "https://chenscode.azurewebsites.net/api/SendMessage",
+        type: "POST",
+        data: JSON.stringify(touchData),
+        crossDomain: true,
+        dataType: "text",
+        contentType: "application/json;charset=utf-8",
+        success: function () {
+            alert("感謝您的建議，我們將會盡速回覆");
+            $('#name').val("");
+            $('#email').val("");
+            $('#subject').val("");
+            $('#msg').val("");
+            $('#btnContactUs').attr('disabled', false);
+        },
+        error: function (xhr, ajaxOptions, thrownError) {
+            alert(xhr.status);
+            alert(thrownError);
+        },
+    });
+    //$.ajax({
+    //    url: "https://chenscode.azurewebsites.net/WeatherForecast",
+    //    type: "GET",
+    //    success: function () {
+    //        alert("YES");
+    //    },
+    //    error: function (xhr, ajaxOptions, thrownError) {
+    //        alert(xhr.status);
+    //    }
+    //})
+});
