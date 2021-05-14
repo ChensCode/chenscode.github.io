1ab86c335d5e6c0811f31a733cb7ee5e923a3071
irving ko <irving.ko@hotmail.com>
2021-05-14 15:13:14 +0800

    first

---
+++ WebSite/Model/TouchData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebSite.Model {
+	public class TouchData {
+		public string Name { get; set; }
+		public string Email { get; set; }
+		public string Subject { get; set; }
+		public string Msg { get; set; }
+	}
+}
